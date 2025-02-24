fe6cdec53bacdab9e0b8f9c0662fe320a7d88873
Thomas Ketterer <thomasketterer3@gmail.com>
2025-02-23 22:43:17 -0500

    Fixing libraries

--- player_tracker/tracker.py
+++ player_tracker/tracker.py
@@ -5,3 +5,8 @@ import seaborn as sns
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
+from torchvision import transforms, datasets
+import torch.nn.functional as F
+from sklearn.metrics import confusion_matrix
+from tqdm import tqdm
+
